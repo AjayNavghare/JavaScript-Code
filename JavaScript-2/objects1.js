69e9ae90ab57fca59fc63ed55ce4899ed8b859f3
AjayNavghare <111764506+AjayNavghare@users.noreply.github.com>
2025-08-07 10:45:07 +0000

    JSON talk

---
+++ JavaScript-2/objects1.js
@@ -0,0 +1,13 @@
+const course = {
+    name : "Ajay",
+    age : 20,
+    courseInspector : "AJ"
+}
+
+const {courseInspector : Inspector} = course //object de-structure
+
+console.log(Inspector);
+
+
+
+//for understanding the format of any json file then go to https://jsonformatter.org/  
