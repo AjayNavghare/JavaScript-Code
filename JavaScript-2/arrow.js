f2220b3574d08d1b2ac81f0b46cfb6661149ea8f
AjayNavghare <111764506+AjayNavghare@users.noreply.github.com>
2025-08-13 14:28:33 +0000

    learn arrow function

---
+++ JavaScript-2/arrow.js
@@ -0,0 +1,10 @@
+// const one = (num1,num2) => {        //explicit function 
+//     return num1 + num2
+// }
+
+const one = (num1,num2) => num1 + num2  //implicit function
+                                        // if you use {} then you need to write return keyword in that  
+
+
+console.log(one(2,4));
+
